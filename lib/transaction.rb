5c4859cbf4136ff7564a844777c0d576d70422a1
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-18 17:16:29 +0100

    Adds test and basic implementation for Transaction#amount

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,6 +3,11 @@ class Transaction
 
   def initialize(date, amount)
     @date = date
+    @amount = amount
+  end
+
+  def amount
+    @amount
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,4 +9,11 @@ describe 'transaction' do
     expect(transaction.date).to eq date
   end
 
+  it '#amount returns transaction amount' do
+    date = Date.new(2012, 1, 10)
+    amount = 1000 * 100
+    transaction = Transaction.new(date, amount)
+    expect(transaction.amount).to eq amount
+  end
+
 end
