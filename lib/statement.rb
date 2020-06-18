f8519fa7a7f5f1278cd01e1c0d578252f922416f
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-18 20:11:36 +0100

    Refactors formatting

--- lib/statement.rb
+++ lib/statement.rb
@@ -6,10 +6,18 @@ class Statement
   end
 
   def self.line(transaction, balance)
-    balance_string = Money.new(balance).format(symbol: false, thousands_separator: '')
-    credit_string = Money.new(transaction.amount).format(symbol: false, thousands_separator: '')
-    date_string = transaction.date.strftime("%d/%m/%Y")
-    "#{date_string} || #{credit_string} || || #{balance_string}"
+    credit = transaction.amount
+
+    "#{format_date(transaction.date)} || #{format_money(credit)} || || #{format_money(balance)}"
   end
 
+  def self.format_money(amount)
+    Money.new(amount).format(symbol: false, thousands_separator: '')
+  end
+
+  def self.format_date(date)
+    date.strftime("%d/%m/%Y")
+  end
+
+  private_class_method :format_money, :format_date
 end
