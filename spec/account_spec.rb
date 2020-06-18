b6ad902dee0b0f801df08bd482569b300f2640fe
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-18 18:08:12 +0100

    Refactors account, amount and transaction into let

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,19 +1,17 @@
 require 'account'
 
 describe 'account' do
+  let(:account) { Account.new }
+
   let(:date) { Date.new(2012, 1, 10) }
-  # let(:amount) { 1000 * 100 }
+  let(:amount) { 1000 * 100 }
+  let(:transaction) { Transaction.new(date, amount) }
 
   it '#balance returns zero on creation' do
-    account = Account.new
     expect(account.balance).to eq 0
   end
 
   it 'Adding a transaction changes the balance by the same amount' do
-    account = Account.new
-    amount = 1000 * 100
-    transaction = Transaction.new(date, amount)
-    
     expect { account.add(transaction) }.to change { account.balance }.by(amount)
   end
 
