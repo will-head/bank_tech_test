dde38989c38f4f4029d44541397d2a7dd13da7c8
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-18 20:33:58 +0100

    Adds test and implementation for debit Statement.line

--- lib/statement.rb
+++ lib/statement.rb
@@ -6,17 +6,28 @@ class Statement
   end
 
   def self.line(transaction, balance)
-    credit = transaction.amount
+    if transaction.amount >= 0
+      credit = transaction.amount
+    else
+      debit = transaction.amount.abs
+    end
 
-    "#{format_date(transaction.date)} || #{format_money(credit)} || || #{format_money(balance)}"
+    date_string = format_date(transaction.date)
+    credit_string = format_money(credit)
+    debit_string = format_money(debit)
+    balance_string = format_money(balance).delete_suffix(' ')
+
+    "#{date_string}||#{credit_string}||#{debit_string}||#{balance_string}"
   end
 
   def self.format_money(amount)
-    Money.new(amount).format(symbol: false, thousands_separator: '')
+    return " " if amount.nil?
+    
+    " #{Money.new(amount).format(symbol: false, thousands_separator: '')} "
   end
 
   def self.format_date(date)
-    date.strftime("%d/%m/%Y")
+    date.strftime("%d/%m/%Y ")
   end
 
   private_class_method :format_money, :format_date
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,6 +8,8 @@ I18n.default_locale = :en
 describe 'statement' do
 
   let(:transaction_1) { Transaction.new(Date.new(2012, 1, 10), 1000 * 100) }
+  let(:transaction_2) { Transaction.new(Date.new(2012, 1, 13), 2000 * 100) }
+  let(:transaction_3) { Transaction.new(Date.new(2012, 1, 14), -500 * 100) }
 
   it 'returns header line' do
     expect(Statement.header).to eq "date || credit || debit || balance"
@@ -15,6 +17,10 @@ describe 'statement' do
 
   it 'returns line for credit' do
     expect(Statement.line(transaction_1, 1000 * 100)).to eq "10/01/2012 || 1000.00 || || 1000.00" 
+  end
+
+  it 'returns line for debit' do
+    expect(Statement.line(transaction_3, 2500 * 100)).to eq "14/01/2012 || || 500.00 || 2500.00" 
   end
 
 end
