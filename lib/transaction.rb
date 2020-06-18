dc9c63f9cc1d7701bba5538c89a5fe3530741661
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-18 17:17:21 +0100

    Refactors #amount into attr_reader

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,13 +1,9 @@
 class Transaction
-  attr_reader :date
+  attr_reader :date, :amount
 
   def initialize(date, amount)
     @date = date
     @amount = amount
-  end
-
-  def amount
-    @amount
   end
 
 end
