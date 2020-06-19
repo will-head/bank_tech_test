4ddef8fb45448a634f3a1bb0008ff837fdd99db2
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-19 12:16:36 +0100

    Adds test and implementation for only passing amount to transaction (test could fail if ran over midnight)

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,7 +2,7 @@ class Transaction
   attr_reader :date, :amount
   attr_accessor :balance
 
-  def initialize(amount, date)
+  def initialize(amount, date = Date.new)
     @date = date
     @amount = amount
     @balance = nil
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,6 +9,12 @@ describe 'transaction' do
     expect(transaction.date).to eq date
   end
 
+  it '#date returns when transcation created, if no date supplied' do
+    now = Date.new
+    transaction = Transaction.new(amount)
+    expect(transaction.date).to eq now
+  end
+
   it '#amount returns transaction amount' do
     transaction = Transaction.new(amount, date)
     expect(transaction.amount).to eq amount
